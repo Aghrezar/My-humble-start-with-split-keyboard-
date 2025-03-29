c3513b752e98564620cfee09ffadffa0e0fbef1b
Aghrezar <57628164+Aghrezar@users.noreply.github.com>
2025-03-29 15:39:15 +0300

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -15,16 +15,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,  KC_I,     KC_O,    KC_P,     KC_MINS,
   KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
   KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,           KC_RBRC, KC_N,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RCTL,
-                        TG_(_NUM), KC_LGUI, KC_LALT, KC_SPC,                         KC_ENT, KC_RALT, KC_GRV, MO_(_FKEY)
+                        TG_(_NUM), KC_LGUI, KC_LALT, KC_SPC,                  KC_ENT,  KC_RALT,  KC_GRV,  MO_(_FKEY)
  ),
 
 
 //num
 [_NUM] = LAYOUT(
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                             KC_PSCR,   KC_DEL,   KC_P7,  KC_P8,  KC_P9,   KC_PAST,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX,  KC_UP,  XXXXXXX,                             KC_INSERT, KC_HOME,  KC_P4,  KC_P5,  KC_P6,   KC_PMNS,
-  _______, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,                             KC_COPY,   KC_END,   KC_P1,  KC_P2,  KC_P3,   KC_PPLS,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WBAK,           KC_WFWD, KC_PSTE,   KC_EXEC,  KC_P0, XXXXXXX, KC_PCMM, KC_PEQL,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                             KC_PSCR,   KC_PGUP, KC_P7,  KC_P8,  KC_P9,   KC_PAST,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX,  KC_UP,  XXXXXXX,                             KC_INSERT, KC_PGDN, KC_P4,  KC_P5,  KC_P6,   KC_PMNS,
+  KC_CAPS, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,                             KC_DEL,    KC_HOME, KC_P1,  KC_P2,  KC_P3,   KC_PPLS,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_COPY,           KC_PSTE, KC_EXEC,   KC_END,  KC_P0, XXXXXXX, KC_PCMM, KC_PEQL,
                         _______, _______, _______, _______,                   _______, _______, _______, MO_(_MOUSE)
 ),
 
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,                              KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  XXXXXXX,
   _______, KC_F21,  KC_F22,  KC_F23,  KC_F24,  XXXXXXX,                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC,           KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                     MO_(_MOUSE), _______, _______, _______,                  _______, _______, _______, _______
+                    MO_(_MOUSE), _______, _______, _______,                  _______, _______, _______, _______
 ),
 
 //mouse
