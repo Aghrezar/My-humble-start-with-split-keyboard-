94eae5b733c52553d40914b656299c7c5d640278
Aghrezar <57628164+Aghrezar@users.noreply.github.com>
2025-04-02 08:35:09 +0300

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -11,11 +11,11 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    
     [_QWERTY] = LAYOUT(
-  KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,   KC_8,     KC_9,    KC_0,     KC_BSPC,
+  QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,   KC_8,     KC_9,    KC_0,     KC_BSPC,
   KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,  KC_I,     KC_O,    KC_P,     KC_MINS,
   KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
   KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,           KC_RBRC, KC_N,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RCTL,
-                        MO(_NUM), KC_LGUI, KC_LALT, KC_SPC,                  KC_ENT,  KC_RALT,  KC_GRV,  MO(_FKEY)
+                  LT(_NUM, KC_LGUI), KC_NO, KC_LALT, KC_SPC,                  KC_ENT,  KC_RALT,  KC_GRV,  MO(_FKEY)
  ),
 
 
@@ -70,8 +70,6 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 bool oled_task_user() {
   // Set cursor position
   if (is_keyboard_left()) {
-  oled_set_cursor(0, 1);
-
   // Switch on current active layer
   switch (get_highest_layer(layer_state)) {
       case _QWERTY :
